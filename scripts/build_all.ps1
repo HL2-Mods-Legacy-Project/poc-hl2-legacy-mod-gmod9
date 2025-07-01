f301a467a8c5da8b09cda0a3f62ddd4789fb628d
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-01 19:26:47 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,10 +1,10 @@
-& "$PSScriptRoot\..\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+& "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFilesPath "$PSScriptRoot\..\game\<mod folder>" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -OutputFilePrefix "GMOD_9.0.4" `
+  -ModName "Garry's Mod 9.0.4" `
+  -ModFilesPath "$PSScriptRoot\..\game\gmod9" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/poc-hl2-legacy-registry/releases/download/gmod9/gmod904-Original.zip" `
+  -ModBaseFilesUrlHash "91ffbbbcc804ccdfe57ef130e9bf0e87302340d4af2d21f739e28fa23d38881d" `
+  -ModReadmePath "readme.txt" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
